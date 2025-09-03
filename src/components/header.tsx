de883f5ed164dc4c9b75a3e37beb603ca37eba6a
Nayeem Islam <z.i.nayeem45@gmail.com>
2025-09-03 09:33:08 +0000

    make some padding in navbar ,,dr eveln reed left side

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import Link from 'next/link';
@@ -39,7 +40,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
+      <div className="container flex h-14 items-center px-4 md:px-6">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="font-bold">Dr. Evelyn Reed</span>
